07a245f297191ae4d61b10c27038d16b549f87be
AnndreLuiz <andre.arqt1@gmail.com>
2022-07-06 20:25:08 -0300

    first commit

---
+++ lib/DetalhesPoke.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+import 'PokemonJson.dart';
+
+class DetalhesPokemons extends StatelessWidget {
+  const DetalhesPokemons({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    PokemonJson detalhes =
+        ModalRoute.of(context)!.settings.arguments as PokemonJson;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          detalhes.name ?? '',
+        ),
+      ),
+      body: Card(
+        margin: const EdgeInsets.only(
+          top: 10,
+          left: 10,
+          right: 10,
+          bottom: 110,
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                width: 400,
+                height: 250,
+                decoration: BoxDecoration(
+                  color: const Color.fromARGB(255, 247, 241, 241),
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: Stack(
+                  alignment: Alignment.topCenter,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Align(
+                        alignment: Alignment.topRight,
+                        child: Text(
+                          '#${detalhes.number}',
+                        ),
+                      ),
+                    ),
+                    Image.network(
+                      '${detalhes.thumbnailImage}',
+                      width: 200,
+                      height: 500,
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Text(
+                '${detalhes.description}',
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              const Text(
+                'Tipo:',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                '${detalhes.type}',
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              const Text(
+                'Fraquezas:',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text('${detalhes.weakness}')
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/HomePage.dart
@@ -0,0 +1,107 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'PokemonJson.dart';
+import 'package:http/http.dart' as http;
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<PokemonJson> pokemonsList = [];
+  bool carregando = true;
+
+  @override
+  void initState() {
+    _pokemonI();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text('Pokedex'),
+            SizedBox(
+              width: 35,
+              child: Image.network(
+                'https://raw.githubusercontent.com/RafaelBarbosatec/SimplePokedex/master/assets/pokebola_img.png',
+              ),
+            ),
+          ],
+        ),
+      ),
+      body: ListView.builder(
+        itemCount: pokemonsList.length,
+        itemBuilder: (context, index) {
+          return Card(
+            margin: const EdgeInsets.all(5),
+            child: InkWell(
+              onTap: () {
+                Navigator.of(context)
+                    .pushNamed('/descricao', arguments: pokemonsList[index]);
+              },
+              child: Padding(
+                padding: const EdgeInsets.all(9.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Image.network(
+                      '${pokemonsList[index].thumbnailImage}',
+                      width: 95,
+                      height: 95,
+                    ),
+                    Column(
+                      children: [
+                        Text(
+                          pokemonsList[index].name ?? '',
+                          style: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 17,
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      width: 125,
+                    ),
+                    Text(
+                      '#${pokemonsList[index].number ?? ''}',
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+
+  void _pokemonI() {
+    Uri url = Uri.parse('http://104.131.18.84/pokemon/?limit=50&page=0');
+    http.get(url).then(
+      (value) {
+        if (value.statusCode == 200) {
+          setState(
+            () {
+              Map json = JsonDecoder().convert(value.body);
+              (json['data'] as List).forEach(
+                (element) {
+                  pokemonsList.add(PokemonJson.fromJson(element));
+                },
+              );
+              carregando = false;
+            },
+          );
+        }
+      },
+    );
+  }
+}
